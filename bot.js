38a5899b4eeebca2c8580c3c32189c064e217c2a
TIGER4EVER <47013568+TIGER4EVER@users.noreply.github.com>
2019-02-05 18:54:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -155,7 +155,7 @@ client.on("message", async message => {
             معلومات عن السيرفر : ${prefix}server
             برودكاست للأونلاين فقط : ${prefix}bco
             يعرض لك عدد المتبندين من سيرفرك : ${prefix}banned
-            رابط سيرفر الدعم الفني : https://discord.gg/BgxH3zf
+            رابط سيرفر الدعم الفني : https://discord.gg/YCkZGrU
             **`);
             message.channel.sendEmbed(help); // رابط السيرفر يعود الى سيرفر CODES .
     }
